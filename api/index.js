eac70daf32a1315a654a3583a5f96e614e790543
JCPrime01 <vanzokiller@gmail.com>
2026-06-29 16:47:52 -0300

    chore: shareCode PV90AL34A2M -> 72AAJ7SMKDC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/spia9nig?shareCode=PV90AL34A2M&afp=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/spia9nig?shareCode=72AAJ7SMKDC&afp=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
